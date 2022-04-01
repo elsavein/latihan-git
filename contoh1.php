2e3dbbf227640b43dd3a26fb695f2ef0d8d75ea9
elsavein <101793121+elsavein@users.noreply.github.com>
2022-04-01 16:24:54 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h1?>";
+		echo "Nama saya Elsa Vein Po'oa saya tinggal di daerah Bekasi olahraga yang saya sukai adalah bola voly";
+	}
+}
---
+++ view-latihan1.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+	<title>Latihan 1</title>
+</head>
+
+<body>
+     Halo kawan.. Yuk kita belajar Web Programing..!!!<br>
+     Nilai 1 = <?= $nilai1; ?> <br>
+     Nilai 2 = <?= $nilai2; ?> <br>
+     ini hasil dari pemodelan dengan method penjumlahan yaitu <?= $nilai1 . " + " . 
+           $nilai2 . " = " . $hasil; ?> 
+</body>
+
+</html>
