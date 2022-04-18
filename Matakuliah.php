95fa18026bb5d5f43832a9a5031e341c0d5e0c16
elsavein <101793121+elsavein@users.noreply.github.com>
2022-04-18 23:58:26 +0700

    Add files via upload

--- Matakuliah.php
+++ Matakuliah.php
@@ -3,23 +3,35 @@ class Matakuliah extends CI_Controller
 {
 
      public function index()
- 
-    {
+     {
 
         $this->load->view('view-form-matakuliah');
-}
-    
-    public function cetak()
-{
 
-        $data  = [
-            'kode' => $this->input->post('kode'),
-            'nama' => $this->input->post('nama'),
-            'sks' => $this->input->post('sks')
+    }
+
+    public function cetak()
+    {
+        $this->form_validation->set_rules('kode', 'kode Matakuliah', ' required|min_leght[3]', [
+                         'required' => 'Kode Matakuliah Harus diisi',
+                         'min_leght' => 'Kode terlalu pendek'
+        ]);
+
+        $this->form_validation->set_rules('nama', 'Nama Matakuliah', 'required|min_leght[3]', [
+                         'required' => 'Nama Matakuliah Harus diisi',
+                         'min_leght' => 'Nama terlalu pendek'
+        ]);
+
+        if ($this->form_validation->run() != true) {
+            $this->load->view('view-form-matakuliah');
+        } else {
+            $data  = [
+                'kode' => $this->input->post('kode'),
+                'nama' => $this->input->post('nama'),
+                'sks' => $this->input->post('sks')
         ];
         
          $this->load->view('view-data-matakuliah', $data);
-
-}
+       }
+  }
 
 }
