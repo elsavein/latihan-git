2d8da08f25c80f6a2860d73030db650bf97845de
elsavein <101793121+elsavein@users.noreply.github.com>
2022-04-19 09:49:40 +0700

    Add files via upload

--- view-form-matakuliah.php
+++ view-form-matakuliah.php
@@ -4,9 +4,12 @@
 <title>Form Input Matakuliah</title>
 
 </head>
+
 <body>
+	<?php echo validation_errors(); ?>
+
      <center>
-        <form action="<?>= base_url('matakuliah/cetak'); ?>" methode="post">
+        <form  action="<?=  base_url('matakuliah/cetak'); ?>" method="post">
         <table>
             <tr>
                
@@ -23,14 +26,14 @@
                  <th>Kode MTK</th>
                  <th>:</th>
                 <td>
-                    <input type="text" name="kode" placeholder="input kode" id="kode">
+                    <input type="text" name="kode" id="kode">
                 </td>
             </tr>
             <tr>
                 <th>Nama MTK</th>
                 <td>:</td>
                 <td>
-                	<input type="text" name="nama" id="nama" placeholder="required">
+                	<input type="text" name="nama" id="nama">
                 </td>
             </tr>
             <tr>
@@ -38,7 +41,7 @@
                 <td>:</td>
                 <td>
                     
-                    <select name="sks">
+                    <select name="sks" id="sks"> 
                         <option value="">Pilih SKS</option>
                         <option value="2">2</option>
                         <option value="3">3</option>
@@ -48,7 +51,6 @@
             </tr>
             <tr>
                 <td colspan="3" align="center">
-
                     <input type="submit" value="Submit">
 
                 </td>
